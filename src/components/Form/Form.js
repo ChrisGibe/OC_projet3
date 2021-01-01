040ae0a1f62f92843b39305aaa85c97a642afbb8
ChrisGibe <gibelli.christophe01@gmail.com>
2021-01-01 14:51:33 +0100

    Test with the select

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -21,24 +21,61 @@ const Form = () => {
               </label>
             </div>
             <div className="form-group">
-              <label className="label" htmlFor={"name"}>Nom</label>
-              <input type="text" id="lastname" name="lastname" className="form-control" required={true}/>
+              <label className="label" htmlFor={"name"}>
+                Nom
+              </label>
+              <input type="text" id="lastname" name="lastname" className="form-control" required={true} />
             </div>
             <div className="form-group">
-              <label className="label" htmlFor={"firstname"}>Prénom</label>
-              <input type="text" id="firstname" name="firstname" className="form-control" required={true}/>
+              <label className="label" htmlFor={"firstname"}>
+                Prénom
+              </label>
+              <input type="text" id="firstname" name="firstname" className="form-control" required={true} />
             </div>
             <div className="form-group form-group-textarea">
-              <label className="label" htmlFor={"message"}>Message</label>
+              <label className="label" htmlFor={"message"}>
+                Message
+              </label>
               <textarea id="message" name="message" className="form-control" rows="6"></textarea>
             </div>
             <div className="form-group">
-              <label className="label" htmlFor={"email"}>Email</label>
-              <input type="email" id="email" name="email" className="form-control" required={true}/>
+              <label className="label" htmlFor={"email"}>
+                Email
+              </label>
+              <input type="email" id="email" name="email" className="form-control" required={true} />
             </div>
             <div className="form-group">
-              <label className="label" htmlFor={"phone"}>Téléphone</label>
-              <input type="text" id="phone" name="phone" className="form-control" required={true}/>
+              <label className="label" htmlFor={"phone"}>
+                Téléphone
+              </label>
+              <input type="text" id="phone" name="phone" className="form-control" required={true} />
+            </div>
+            <div className="form-group">
+              <label className="label" htmlFor={"seance"}>
+                Séance
+              </label>
+              <select type="select" id="seance" name="seance" className="form-control" required={true}>
+                <option value="Le professionnel 05/08 18h">Le professionnel 05/08 18h</option>
+                <option value="Léon 05/08 21h">Léon 05/08 21h</option>
+                <option value="Tchao pantin 06/08 18h">Tchao pantin 06/08 18h</option>
+                <option value="La chèvre 06/08 21h">La chèvre 06/08 21h</option>
+                <option value="Garde à vue 07/08 18h">Garde à vue 07/08 18h</option>
+                <option value="Le nom de la rose 07/08 21h">Le nom de la rose 07/08 21h</option>
+                <option value="Le nom de la rose 08/08 18h">Le nom de la rose 08/08 18h</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label className="label" htmlFor={"number"}>
+                Nombre de personnes
+              </label>
+              <select type="select" id="number" name="number" className="form-control" required={true}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+              </select>
             </div>
             <div className="full flex flex-end">
               <button className="btn btn-primary" type="submit">
